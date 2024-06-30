349985f8269b9ac2df0f49015a2c49e3949d6af3
szhanggg <stevez9101@gmail.com>
2024-06-30 14:32:53 -0400

    fixed apply page

--- src/app/components/ApplyLayout.tsx
+++ src/app/components/ApplyLayout.tsx
@@ -1,23 +1,25 @@
 export default function ApplyLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <div>
-        <div className="w-full flex flex-col items-center relative py-48 px-8 text-center">
-            <h1 className="md:text-7xl text-5xl font-bold text-shadow">Apply</h1>
-            <div className="absolute inset-0 -z-10 opacity-50">
-                <img
-                src="/images/backgrounds/grow.jpg"
-                className="w-full h-full object-cover blur-sm"
-                alt="Background"
-                />
-            </div>
-        </div>
-        <div className="mt-[-10vh] mx-8 md:mx-40 mb-24">
-            {children}
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <div className="w-full flex flex-col items-center relative py-48 px-8 text-center relative">
+        <h1 className="md:text-7xl text-5xl font-bold text-shadow z-20">
+          Apply
+        </h1>
+        <div className="absolute inset-0 opacity-50 z-10">
+          <img
+            src="/images/backgrounds/grow.jpg"
+            className="w-full h-full object-cover blur-sm"
+            alt="Background"
+          />
         </div>
       </div>
-    );
-  }
+      <div className="mt-[-10vh] mx-8 md:mx-40 mb-24 z-20 relative">
+        {children}
+      </div>
+    </div>
+  );
+}
