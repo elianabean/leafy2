9b81766b507e4df016dfd62985c811f0b87c38c4
szhanggg <stevez9101@gmail.com>
2024-06-30 15:31:27 -0400

    comments

--- src/app/components/index/First.tsx
+++ src/app/components/index/First.tsx
@@ -1,74 +1,69 @@
+// Author: Eliana Wang
+// Data: 2023-12-17
+// Description: Hero page of the website
+// This section contains the name of the company, a very brief description, two buttons, and a 3D model of a plant.
+
 import Model from "../model/Scene";
-import Image from 'next/image'
-import {Button} from "@nextui-org/button";
+import { Button } from "@nextui-org/button";
 import Link from 'next/link'
 
 export default function First() {
     return (
-    <div className="w-[100vw]">
-        <div className="absolute ">
-        <div className="h-[90svh] relative w-[100vw] flex flex-col justify-center overflow-x-hidden font-semibold overflow-y-hidden md:overflow-y-auto "> 
-            <div
-                className="absolute -top-16 -right-16 md:top-1/4 md:right-1/4 w-72 h-72 bg-[radial-gradient(169.40%_89.55%_at_94.76%_6.29%,rgba(63,169,41,0.8)_0%,rgba(255,255,255,0.00)_100%)] md:bg-[radial-gradient(169.40%_89.55%_at_94.76%_6.29%,rgba(63,169,41,0.6)_0%,rgba(255,255,255,0.00)_100%)] mix-blend-multiply rounded-full filter blur-xl opacity-30 animate-blob"
-            >
-            </div>
-            <div
-                className="absolute -bottom-16 -left-16 md:top-1/3 md:left-4 w-72 h-72 bg-[radial-gradient(169.40%_89.55%_at_94.76%_6.29%,rgba(63,169,41,0.8)_0%,rgba(255,255,255,0.00)_100%)] md:bg-[radial-gradient(169.40%_89.55%_at_94.76%_6.29%,rgba(63,169,41,0.6)_0%,rgba(255,255,255,0.00)_100%)] mix-blend-multiply rounded-full filter blur-xl opacity-30 animate-blob animation-delay-2000"
-            >
-            </div>
-        </div>
-        </div>
+        <div className="w-[100vw]">
 
-        <div
-        className="h-[90svh] relative flex flex-col md:flex-row justify-end items-center px-4 md:px-0"
-        >
-            {/* Main Section Contents */}
-            <div
-                className="md:pl-[10vw] flex flex-col items-center justify-center md:items-start md:w-[50vw] text-center md:text-start h-[30vh] md:h-fit"
-            >
-                <h1
-                className="w-full md:text-7xl text-5xl text-shadow font-bold tracking-tight font-workSans"
-                data-aos="fade-right"
-                data-aos-delay="500"
-                data-aos-duration="2000"
-                >
-                Plant your
-                </h1>
-                <h1
-                className="md:text-7xl text-5xl text-primary text-shadow font-bold tracking-tight"
-                data-aos="fade-right"
-                data-aos-delay="750"
-                data-aos-duration="2000"
-                >
-                future
-                </h1>
-                <p
-                className="text-lg whitespace-normal mt-4 md:pr-32 "
-                data-aos="fade-up"
-                data-aos-delay="1250"
-                data-aos-duration="2000"
-                >
-                Industry leaders in house plant development and maintenance.
-                </p>
-                <div className="mt-4 flex space-x-4 font-normal text-[18px]">
-                <Link
-                    href="/careers"
-                    data-aos="fade-up"
-                    data-aos-duration="2000"
-                    data-aos-delay="1750"><Button color="primary" className="text-black bg-white shadow-md shadow-gray-300 text-[18px] px-4 py-6" radius="md">Explore Careers</Button>
-                    </Link>
-                <Link
-                    href="/growwithus"
-                    data-aos="fade-up"
-                    data-aos-duration="2000"
-                    data-aos-delay="2000"><Button color="primary" className="px-4 py-6 bg-primary text-white shadow-md shadow-gray-300 text-[18px]" radius="md">Apply Now</Button></Link>
+            {/* Background Blobs */}
+            <div className="absolute ">
+                <div className="h-[90svh] relative w-[100vw] flex flex-col justify-center overflow-x-hidden font-semibold overflow-y-hidden md:overflow-y-auto ">
+                    <div
+                        className="absolute -top-16 -right-16 md:top-1/4 md:right-1/4 w-72 h-72 bg-[radial-gradient(169.40%_89.55%_at_94.76%_6.29%,rgba(63,169,41,0.8)_0%,rgba(255,255,255,0.00)_100%)] md:bg-[radial-gradient(169.40%_89.55%_at_94.76%_6.29%,rgba(63,169,41,0.6)_0%,rgba(255,255,255,0.00)_100%)] mix-blend-multiply rounded-full filter blur-xl opacity-30 animate-blob"
+                    >
+                    </div>
+                    <div
+                        className="absolute -bottom-16 -left-16 md:top-1/3 md:left-4 w-72 h-72 bg-[radial-gradient(169.40%_89.55%_at_94.76%_6.29%,rgba(63,169,41,0.8)_0%,rgba(255,255,255,0.00)_100%)] md:bg-[radial-gradient(169.40%_89.55%_at_94.76%_6.29%,rgba(63,169,41,0.6)_0%,rgba(255,255,255,0.00)_100%)] mix-blend-multiply rounded-full filter blur-xl opacity-30 animate-blob animation-delay-2000"
+                    >
+                    </div>
                 </div>
             </div>
-            <div className="relative md:right-0 md:top-0 h-[30vh] flex flex-col justify-center md:justify-center z-20  ">
-                <Model></Model>
+
+            <div
+                className="h-[90svh] relative flex flex-col md:flex-row justify-end items-center px-4 md:px-0"
+            >
+                {/* Main Section Contents */}
+                <div
+                    className="md:pl-[10vw] flex flex-col items-center justify-center md:items-start md:w-[50vw] text-center md:text-start h-[30vh] md:h-fit"
+                >
+                    <h1
+                        className="w-full md:text-7xl text-5xl text-shadow font-bold tracking-tight font-workSans"
+                    >
+                        Plant your
+                    </h1>
+                    <h1
+                        className="md:text-7xl text-5xl text-primary text-shadow font-bold tracking-tight"
+                    >
+                        future
+                    </h1>
+                    <p
+                        className="text-lg whitespace-normal mt-4 md:pr-32 "
+                    >
+                        Industry leaders in house plant development and maintenance.
+                    </p>
+                    <div className="mt-4 flex space-x-4 font-normal text-[18px]">
+                        <Link
+                            href="/careers"
+                        ><Button color="primary" className="text-black bg-white shadow-md shadow-gray-300 text-[18px] px-4 py-6" radius="md">Explore Careers</Button>
+                        </Link>
+                        <Link
+                            href="/growwithus"
+                        ><Button color="primary" className="px-4 py-6 bg-primary text-white shadow-md shadow-gray-300 text-[18px]" radius="md">Apply Now</Button></Link>
+                    </div>
+                </div>
+
+                {/* 3D Plant Model */}
+                <div className="relative md:right-0 md:top-0 h-[30vh] flex flex-col justify-center md:justify-center z-20  ">
+                    <Model></Model>
+                </div>
             </div>
+
         </div>
-        
-    </div>
     )
 }
--- src/app/components/model/Scene.tsx
+++ src/app/components/model/Scene.tsx
@@ -1,14 +1,20 @@
+// Author: Eliana Wang
+// Date: 2024-5-30
+// Description: The 3D Model of the plant
 'use client';
 import { Canvas } from "@react-three/fiber"
-import {Model} from "./Plant"
-import {OrbitControls} from "@react-three/drei";
+import { Model } from "./Plant"
+import { OrbitControls } from "@react-three/drei";
 
 export default function index() {
     return (
         <div>
             <div className="w-[100vw] h-[20vh] md:w-[50vw] md:h-[60vh] lg:h-[80vh] flex flex-col justify-center items-center ">
                 <Canvas >
+                    {/* Orbital controls enable the user to rotate the model */}
                     <OrbitControls enableZoom={false} enablePan={false}  ></OrbitControls>
+                    {/* Ambient light is a light that affects all objects in the scene equally */}
+                    {/* Directional light is a light that is emitted from a specific direction */}
                     <ambientLight intensity={8} />
                     <directionalLight position={[-2, 1, 1]} intensity={3} />
                     <Model />
